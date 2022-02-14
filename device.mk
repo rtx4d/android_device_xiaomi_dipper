8b80e7b098729a702b2719afc340a8173408c87e
Andrey Sprynga <rtx4d@aospa.co>
2022-02-14 15:07:50 +0700

    dipper: Uprev fingerprint HAL to 2.3
    Signed-off-by: Andrey Sprynga <rtx4d@aospa.co>

--- device.mk
+++ device.mk
@@ -1,6 +1,6 @@
 #
 # Copyright (C) 2018-2020 The LineageOS Project
-# Copyright (C) 2020-2021 Paranoid Android
+# Copyright (C) 2020-2022 Paranoid Android
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -53,7 +53,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_sdm845
+    android.hardware.biometrics.fingerprint@2.3-service.xiaomi_sdm845
 
 # GPS
 PRODUCT_COPY_FILES += \
